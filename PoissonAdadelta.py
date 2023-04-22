071c59d73a82a33c7c0084c76cb5e78923666661
Bartosz Janowski <detnotatka@gmail.com>
2023-04-22 18:16:32 +0200

    Initial commit

---
+++ PoissonAdadelta.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import numpy as np
+import mnist
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+from matplotlib import pyplot
+from keras.utils import to_categorical
+
+from tensorflow import keras
+from keras import backend as K
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1, 784))
+
+model = Sequential([
+  Dense(64, activation='relu', input_shape=(784,)),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax'),
+])
+
+# Poisson
+# model.compile(
+#   optimizer='adam',
+#   loss='poisson',
+#   metrics=['accuracy'],
+# )
+
+# Adadelta
+model.compile(
+  optimizer='adadelta',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+# This will stop the model if it stops improving to avoid overfitting
+f1 = EarlyStopping(monitor='val_loss', mode='min', verbose=2, patience=10)
+
+# Used to save the model at certain intervals during training
+f2 = ModelCheckpoint(filepath="adadelta_model.h5", monitor='val_loss', mode='min', verbose=2, save_best_only=True)
+
+history = model.fit(
+  train_images,
+  to_categorical(train_labels),
+  epochs=100,
+  batch_size=30,
+  validation_data=(test_images, to_categorical(test_labels)),
+  verbose=1,
+  callbacks=[f1, f2]
+).history
+
+print(model.summary())
+
+# NOTE: same name as in ModelCheckpoint
+trained_model = load_model("adadelta_model.h5")
+
+train_loss, train_accuracy = trained_model.evaluate(train_images, to_categorical(train_labels))
+# TASK 8
+print("Training loss:", train_loss)
+print("Training accuracy:", train_accuracy)
+
+test_loss, test_accuracy = trained_model.evaluate(test_images, to_categorical(test_labels))
+# TASK 8
+print("Testing loss:", train_loss)
+print("Testing accuracy:", train_accuracy)
+
+# Plot the training and validation accuracy
+pyplot.plot(history['accuracy'])
+pyplot.plot(history['val_accuracy'])
+pyplot.title('Model accuracy')
+pyplot.ylabel('Accuracy')
+pyplot.xlabel('Epoch')
+pyplot.legend(['Train', 'Validation'], loc='upper left')
+# saving the image
+pyplot.savefig('adadelta_accuracy_plot.png')
+pyplot.show()
+
+# Plot the training and validation loss
+pyplot.plot(history['loss'])
+pyplot.plot(history['val_loss'])
+pyplot.title('Model loss')
+pyplot.ylabel('Loss')
+pyplot.xlabel('Epoch')
+pyplot.legend(['Train', 'Validation'], loc='upper left')
+# saving the image
+pyplot.savefig('adadelta_loss_plot.png')
+pyplot.show()
+
+
+'''
+TASK 1
+The results I got with "poisson" are fairly close to the previous ones
+
+Task 2
+Yes I see changes in how the model behaved compared to the "adam" algorithm. Adadelta, with the current model, needed A LOT of epochs and never got near
+the accuracy or loss of the Adam algorithm for our model.
+
+The reason for this is because Adam adjusts the learning rate based on the first and second moments of the gradients while Adadelta only uses the first moment
+and scales based on running average of recent gradients, so it did improve a steady improvement curve while overall performing worse with our model, at
+least with the data we used
+'''
